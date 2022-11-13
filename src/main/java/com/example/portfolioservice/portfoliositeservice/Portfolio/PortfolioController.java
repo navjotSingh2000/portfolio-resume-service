ffa34004e4c98cfa9a65a27dc2438f44df06b7eb
Navjot Singh <navjotsingh@Navjots-MacBook-Air.local>
2022-11-13 00:45:40 -0500

    removed ip finder

--- src/main/java/com/example/portfolioservice/portfoliositeservice/Portfolio/PortfolioController.java
+++ src/main/java/com/example/portfolioservice/portfoliositeservice/Portfolio/PortfolioController.java
@@ -1,6 +1,5 @@
 package com.example.portfolioservice.portfoliositeservice.Portfolio;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -20,14 +19,8 @@ import java.nio.file.Paths;
 @CrossOrigin
 public class PortfolioController {
 
-    @Autowired
-    private IPAddressFinder ipAddressFinder;
-
     @GetMapping
     public ResponseEntity<Resource> download(HttpServletRequest request) throws IOException {
-
-        String clientIp = ipAddressFinder.getClientIp(request);
-        System.out.println("clientIp: " + clientIp);
 
         ClassPathResource res = new ClassPathResource("src/main/resources/resume-navjot-singh.pdf");
         File file = new File(res.getPath());
@@ -37,7 +30,6 @@ public class PortfolioController {
         header.add("Cache-Control", "no-cache, no-store, must-revalidate");
         header.add("Pragma", "no-cache");
         header.add("Expires", "0");
-        header.add("IP Address", clientIp);
 
         Path path = Paths.get(file.getAbsolutePath());
         ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
